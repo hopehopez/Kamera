c19c80ed42693f7c06c9643f211fdef5b7e97fc9
zhangshuqing <zhangshuqing912@163.com>
2020-09-16 17:30:04 +0800

    init

---
+++ Kamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kamera
+//
+//  Created by zsq on 2020/9/16.
+//  Copyright © 2020 zsq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
